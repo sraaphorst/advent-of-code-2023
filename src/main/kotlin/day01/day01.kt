da434906cf90281b3c32b50105261852e7c370cc
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-11-30 23:43:35 -1000

    Fixed day 1.

--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -4,48 +4,48 @@
 package day01
 
 private fun processDigits(lines: List<String>): Int =
+    // This could greatly be optimized. We can stop after first digit in either direction.
     lines.sumOf { line ->
         val digits = line.filter(Char::isDigit)
-        val firstDigit = Character.getNumericValue(digits.first())
-        val lastDigit = Character.getNumericValue(digits.last())
-        println("Line: $line, Digits: $digits, $firstDigit$lastDigit")
-        10 * firstDigit + lastDigit
+        10 * digits.first().digitToInt() + digits.last().digitToInt()
     }
 
 fun answer1(input: String): Int =
     processDigits(input.trimIndent().lines())
 
 private val replaceWords = listOf(
-    "one" to "1",
-    "two" to "2",
-    "three" to "3",
-    "four" to "4",
-    "five" to "5",
-    "six" to "6",
-    "seven" to "7",
-    "eight" to "8",
-    "nine" to "9"
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9
 )
 
-// Process the line to generate a new line that meets the criteria.
-private tailrec fun processor(line: String, newLine: String = ""): String =
-    if (line.isEmpty()) newLine
-    else if (line.first().isDigit()) processor(line.drop(1), newLine + line.first())
-    else {
-        val word = replaceWords.find { w -> line.startsWith(w.first) }
-        if (word != null)
-            processor(line.removePrefix(word.first), newLine + word.second)
-        else
-            processor(line.drop(1), newLine + line.first())
-    }
+fun answer2(input: String): Int {
+    val lines = input.trimIndent().lines()
 
-fun answer2(input: String): Int =
-    processDigits(input.trimIndent().lines().map{line ->
-        val abc = processor(line)
-        println("Old: $line")
-        println("New: $abc")
-        abc
-    })
+    // Find the first digit in each line.
+    fun auxFirst(remain: String): Int =
+        if (remain.first().isDigit()) remain.first().digitToInt()
+        else {
+            val word = replaceWords.find { w -> remain.startsWith(w.first) }
+            word?.second ?: auxFirst(remain.drop(1))
+        }
+
+    // Find the last digit in each line.
+    fun auxLast(remain: String): Int =
+        if (remain.last().isDigit()) remain.last().digitToInt()
+        else {
+            val word = replaceWords.find { w -> remain.endsWith(w.first) }
+            word?.second ?: auxLast(remain.dropLast(1))
+        }
+
+    return lines.sumOf { l -> 10 * auxFirst(l) + auxLast(l) }
+}
 
 
 fun main() {
@@ -53,7 +53,7 @@ fun main() {
     println("--- Day1: Trebuchet! ---")
 
 //    // Answer 1: 55621
-//    println("Part 1: ${answer1(input)}")
+    println("Part 1: ${answer1(input)}")
 
     // Answer 2: 53587 is too low... should be 53592.
     print("Part 2: ${answer2(input)}")
