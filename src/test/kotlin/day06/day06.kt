ec76a621120c6a5f6da55f464c273993a02d00a1
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-12-05 22:43:52 -1000

    Day 6 small cleanup.

--- src/test/kotlin/day06/day06.kt
+++ src/test/kotlin/day06/day06.kt
@@ -3,24 +3,51 @@
 
 package day06
 
-import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import kotlin.math.ceil
+import kotlin.math.sqrt
 
-class Day06Test {
-    companion object {
-        private val input = """
-            Time:      7  15   30
-            Distance:  9  40  200
-        """.trimIndent()
-    }
+// We want to BEAT the record, so we need to add a tiny epsilon to distance to eliminate the case that we tie
+// the record.
+private val epsilon = 1e-10
 
-    @Test
-    fun `Problem 1 example`() {
-        assertEquals(288, answer1(input))
-    }
+// To get the answer, we want to solve the monic quadratic polynomial:
+// x(t-x) = d -> -x^2 + tx - d = 0 -> x^2 - tx + d = 0
+private fun solveMonicQuadratic(b: Double, c: Double): Pair<Double, Double> {
+    // We assume that the discriminant is >= 0.
+    val discriminant = b * b - 4 * c
+    val sqrtDiscriminant = sqrt(discriminant)
+    return (-b + sqrtDiscriminant) / 2.0 to (-b - sqrtDiscriminant) / 2.0
+}
 
-    @Test
-    fun `Problem 2 example`() {
-        assertEquals(71503, answer2(input))
-    }
+fun answer1(input: String): Long =
+    input
+        .trim()
+        .lines()
+        .map { it.split(':')[1].trim().split(' ').mapNotNull(String::toLongOrNull) }
+        .let { it[0].zip(it[1]) }
+        .fold(1L){ acc, (t, d) ->
+            val (r1, r2) = solveMonicQuadratic(-t.toDouble(), d.toDouble() + epsilon)
+            val diff = ceil(r1).toLong() - ceil(r2).toLong()
+            acc * diff
+        }
+
+fun answer2(input: String): Long {
+    val (t, d) = input
+        .trim()
+        .lines()
+        .map { it.split(':')[1].replace(" ", "").let(String::toLong) }
+    val (r1, r2) = solveMonicQuadratic(-t.toDouble(), d.toDouble() + epsilon)
+    return ceil(r1).toLong() - ceil(r2).toLong()
+}
+
+fun main() {
+    val input = object {}.javaClass.getResource("/day06.txt")!!.readText()
+
+    println("--- Day 6: Wait For It ---")
+
+    // Answer 1: 252000
+    println("Part 1: ${answer1(input)}")
+
+    // Answer 2: 36992486
+    println("Part 2: ${answer2(input)}")
 }
