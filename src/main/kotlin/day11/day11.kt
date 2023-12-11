97c5a7f848148727a14a4148bb51445c8d5349c1
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-12-10 21:38:06 -1000

    Day 11 initial solution.

--- src/main/kotlin/day11/day11.kt
+++ src/main/kotlin/day11/day11.kt
@@ -20,12 +20,11 @@ private data class Galaxy(val id: Int, val coordinates: Coord) {
                 .map(IndexedValue<String>::index)
                 .toSet()
 
-        fun parse(input: String, emptyValue: Int = 2): Sequence<Galaxy> {
+        fun parse(input: String, emptyValue: Long = 2): List<Galaxy> {
             val rows = input
                 .trim()
                 .lines()
 
-            println("Length: ${rows.first().length}")
             val cols = (0..<rows.first().length).map { colIdx ->
                 rows.joinToString(separator = "") { it[colIdx].toString() }
             }
@@ -33,49 +32,30 @@ private data class Galaxy(val id: Int, val coordinates: Coord) {
             val emptyRows = emptyIndices(rows)
             val emptyCols = emptyIndices(cols)
 
-            val g = rows.withIndex().flatMap { (rIdx, row) ->
-                row.withIndex().mapNotNull { (cIdx, ch) -> if (ch == '#') (rIdx to cIdx) else null }
-            }
-            println("Cols: $cols")
-            println("Empty rows: $emptyRows")
-            println("Empty cols: $emptyCols")
-            println("--- ROWS ---")
-            g.forEach { println(it) }
-            println("------------")
-
             // Parse the galaxies.
-            return rows.asSequence().withIndex().flatMap { (rIdx, row) ->
+            return rows.withIndex().flatMap { (rIdx, row) ->
                 row.withIndex().mapNotNull { (cIdx, ch) -> if (ch == '#') (rIdx to cIdx) else null }
             }.withIndex().map { (idx, coordinates) ->
                 val (rIdx, cIdx) = coordinates
                 // Determine the adjustment to the coordinates.
-                println("Processing ${idx + 1}: $coordinates")
-                val er = emptyRows.filter { it <= rIdx }
-                val ec = emptyCols.filter { it <= cIdx }
-                println("\temptyRows: ${er.size} -> $er")
-                println("\temptyCols: ${ec.size} -> $ec")
-
                 val adjustedCoordinates = Coord(
                     rIdx.toLong() + (emptyValue - 1) * emptyRows.count { it <= rIdx },
                     cIdx.toLong() + (emptyValue - 1) * emptyCols.count { it <= cIdx }
                 )
-                val g = Galaxy(idx + 1, adjustedCoordinates)
-                println("\t${idx + 1}: $coordinates -> $adjustedCoordinates")
-                g
+                Galaxy(idx + 1, adjustedCoordinates)
             }
         }
     }
 }
 
-private fun <T> Sequence<T>.uniquePairs(): Sequence<Pair<T, T>> = sequence {
-    this@uniquePairs.forEachIndexed { idx, a ->
-        this@uniquePairs.drop(idx + 1).forEach { b ->
-            yield(a to b)
-        }
+private fun <T> List<T>.uniquePairs(): List<Pair<T, T>> =
+    indices.flatMap { i ->
+        ((i + 1)..<size).map { j ->
+            this[i] to this[j]
     }
 }
 
-private fun answer(input: String, emptyDistance: Int): Long =
+fun answer(input: String, emptyDistance: Long): Long =
     Galaxy.parse(input, emptyDistance)
         .uniquePairs()
         .fold(0L) { acc, pair -> acc + (pair.first - pair.second) }
@@ -83,16 +63,17 @@ private fun answer(input: String, emptyDistance: Int): Long =
 fun answer1(input: String): Long =
     answer(input, 2)
 
-fun answer2(input: String): Int = TODO()
+fun answer2(input: String): Long =
+    answer(input, 1_000_000)
 
 fun main() {
     val input = object {}.javaClass.getResource("/day11.txt")!!.readText()
 
     println("--- Day 11: Cosmic Expansion ---")
 
-    // Answer 1: 6820
+    // Answer 1: 9521550
     println("Part 1: ${answer1(input)}")
 
-    // Answer 2: 337
-//    println("Part 2: ${answer2(input)}")
+    // Answer 2: 298932923702
+    println("Part 2: ${answer2(input)}")
 }
