eb54e679451a810441867c5b44bf3baec094bf47
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-12-14 01:17:34 -1000

    Update day13.kt

--- src/main/kotlin/day13/day13.kt
+++ src/main/kotlin/day13/day13.kt
@@ -8,9 +8,9 @@ private typealias RowData = Data
 private typealias ColData = Data
 private typealias Terrain = Pair<RowData, ColData>
 
-private enum class LineReflectionType {
-    LineOfReflection,
-    SmudgedLineOfReflection,
+private enum class ReflectionType {
+    Reflection,
+    SmudgedReflection,
     NoReflection
 }
 
@@ -18,7 +18,7 @@ private enum class LineReflectionType {
 // if it is a normal line of reflection or a smudged one.
 // This could be optimized somewhat (e.g. by passing the required LineReflectionType here),
 // but this works sufficiently fast.
-private fun checkLine(data: Data, line: Int): LineReflectionType {
+private fun checkLine(data: Data, line: Int): ReflectionType {
     // Check if two Longs differ in exactly one bit, which represents a reflection with smudge.
     fun smudgedReflection(l1: Long, l2: Long): Boolean {
         val l = l1 xor l2
@@ -26,11 +26,11 @@ private fun checkLine(data: Data, line: Int): LineReflectionType {
     }
 
     // At iteration idx, we compare lines (line - idx) and (line + idx + 1).
-    tailrec fun aux(idx: Int = 0, smudgeSeen: Boolean = false): LineReflectionType = when {
+    tailrec fun aux(idx: Int = 0, smudgeSeen: Boolean = false): ReflectionType = when {
         // Stop if we reach line + 1 or if the reflected line does not exist.
         idx == line + 1 || line + idx + 1 >= data.size ->
-            if (smudgeSeen) LineReflectionType.SmudgedLineOfReflection
-            else LineReflectionType.LineOfReflection
+            if (smudgeSeen) ReflectionType.SmudgedReflection
+            else ReflectionType.Reflection
 
         // Lines reflect and no smudge here. Propagate smudgeSeen.
         data[line - idx] == data[line + idx + 1] ->
@@ -41,7 +41,7 @@ private fun checkLine(data: Data, line: Int): LineReflectionType {
             aux(idx + 1, true)
 
         // Either lines do not reflect, or reflect with a smudge and we have already seen one.
-        else -> LineReflectionType.NoReflection
+        else -> ReflectionType.NoReflection
     }
 
     return aux()
@@ -50,8 +50,10 @@ private fun checkLine(data: Data, line: Int): LineReflectionType {
 // Check to see if there is a line of reflection of the type required.
 // If there is, we want to add 1 to indicate how many lines are before the line.
 // If not, we return null.
-private fun checkData(data: Data, lineReflectionType: LineReflectionType): Int? =
-    (0..(data.size - 2)).firstOrNull { checkLine(data, it) == lineReflectionType }?.let { it + 1 }
+private fun checkData(data: Data, reflectionType: ReflectionType): Int? =
+    (0..(data.size - 2))
+        .firstOrNull { checkLine(data, it) == reflectionType }
+        ?.let { it + 1 }
 
 // Check the rows and columns for the required type of line of reflection.
 // 1. terrain.first are the rows, so if a value is returned by checkData, there are that many rows above it; and
@@ -59,8 +61,8 @@ private fun checkData(data: Data, lineReflectionType: LineReflectionType): Int? 
 // As per the puzzle description:
 // 1. return 100 * the number of rows above a line of horizontal line of reflection; or
 // 2. return the number of columns to the left of a line of reflection.
-private fun checkTerrain(terrain: Terrain, lineReflectionType: LineReflectionType): Int =
-    (checkData(terrain.first, lineReflectionType) ?: 0) * 100 + (checkData(terrain.second, lineReflectionType) ?: 0)
+private fun checkTerrain(terrain: Terrain, reflectionType: ReflectionType): Int =
+    (checkData(terrain.first, reflectionType) ?: 0) * 100 + (checkData(terrain.second, reflectionType) ?: 0)
 
 // Convert a string of # and . to a Long corresponding to 1 and 0 respectively.
 private fun stringToLong(bitString: String): Long =
@@ -81,10 +83,10 @@ private fun parse(input: String): List<Terrain> =
     }
 
 fun answer1(input: String): Int =
-    parse(input).sumOf { checkTerrain(it, LineReflectionType.LineOfReflection) }
+    parse(input).sumOf { checkTerrain(it, ReflectionType.Reflection) }
 
 fun answer2(input: String): Int =
-    parse(input).sumOf { checkTerrain(it, LineReflectionType.SmudgedLineOfReflection) }
+    parse(input).sumOf { checkTerrain(it, ReflectionType.SmudgedReflection) }
 
 fun main() {
     val input = object {}.javaClass.getResource("/day13.txt")!!.readText()
